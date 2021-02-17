f12646d9992eb30408cf8412395405e03b608e0e
rottbers <johan@rottbers.com>
2021-02-17 19:12:19 +0100

    Updated build process

--- web/next.config.js
+++ web/next.config.js
@@ -2,4 +2,22 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
+  rewrites: async () =>
+    process.env.NODE_ENV === 'development'
+      ? [
+          {
+            source: '/studio/:path*',
+            destination: 'http://localhost:3333/studio/:path*',
+          },
+          {
+            source: '/__webpack_hmr',
+            destination: 'http://localhost:3333/__webpack_hmr',
+          },
+        ]
+      : [
+          {
+            source: '/studio/:path*',
+            destination: '/studio/index.html',
+          },
+        ],
 };
