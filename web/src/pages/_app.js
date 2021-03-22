7a0c04a556a5aa3e6598ad396336ca40e9d05a13
rottbers <johan@rottbers.com>
2021-03-22 14:11:42 +0100

    Fixed some lighthouse issues

--- web/src/pages/_app.js
+++ web/src/pages/_app.js
@@ -9,23 +9,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <title>KYH Onboarding</title>
         {/* TODO: consider making a setting in the CMS, fixed URL for now */}
