eb0865a7a04d049a47948670b337870da99f9290
rottbers <johan@rottbers.com>
2021-03-25 14:46:23 +0100

    Adds loading page to topics page

--- web/src/components/LoadingPage.js
+++ web/src/components/LoadingPage.js
@@ -1,12 +1,12 @@
 import Logo from './Logo';
 
 const LoadingPage = () => (
-  <main className="h-screen flex flex-col items-center justify-center">
+  <div className="h-screen flex flex-col items-center justify-center">
     <Logo className="w-32 h-32 animate-pulse" aria-label="KYH" />
     <p className="sr-only" role="status">
       Laddar
     </p>
-  </main>
+  </div>
 );
 
 export default LoadingPage;
